3e05a17f987f9a75cd53859f3ca7a6ef9c1ca717
Muhammad Umer <m.umer.17865@khi.iba.edu.pk>
2024-08-28 17:31:55 +0500

    backend | first-Commit2

--- sequelize.js
+++ sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  dialect: 'mysql',
 });
 
 module.exports = sequelize;
