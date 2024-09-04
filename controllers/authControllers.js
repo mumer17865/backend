171457155bcf00f900c56560d08e3a0bff958ec9
Muhammad Umer <m.umer.17865@khi.iba.edu.pk>
2024-09-04 18:16:25 +0500

    bsecureOrderchange2589

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const { generateToken } = require('../utils/auth');
-const Data = require('../models/data');
+const Data1 = require('../models/data');
 const { QueryTypes } = require('sequelize');
 const sequelize = require('../sequelize');
 
@@ -18,7 +18,7 @@ exports.register = async (req, res) => {
         ...req.body,
         pass: hash,
       };
-      const data = await Data.create(data1);
+      const data = await Data1.create(data1);
       res.send({
         success: true,
         data: data,
@@ -46,7 +46,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { uname, pass } = req.body;
-    const user = await Data.findOne({ where: { uname } });
+    const user = await Data1.findOne({ where: { uname } });
     if (!user) {
       res.json({ success: false });
       return;
